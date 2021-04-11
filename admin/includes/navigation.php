682772fd3400e7d30988154f9ced853f88d87fae
ahmed_khaled1 <ahmedkhaledmohamed9@gmail.com>
2021-04-11 23:13:33 +0200

    display the content of posts table from the DB

--- admin/includes/navigation.php
+++ admin/includes/navigation.php
@@ -42,15 +42,15 @@
                         <a href="javascript:;" data-toggle="collapse" data-target="#posts_dropdown"><i class="fa fa-fw fa-arrows-v"></i> Posts <i class="fa fa-fw fa-caret-down"></i></a>
                         <ul id="posts_dropdown" class="collapse">
                             <li>
-                                <a href="#">View All Posts</a>
+                                <a href="./posts.php">View All Posts</a>
                             </li>
                             <li>
                                 <a href="#">Add Posts</a>
                             </li>
-                        </ul>
+                        </ul> 
                     </li>
                     <li>
-                        <a href="bootstrap-grid.html"><i class="fa fa-fw fa-wrench"></i> Gategories </a>
+                        <a href="./categories.php"><i class="fa fa-fw fa-wrench"></i> Gategories </a>
                     </li>
                     <li class="active">
                         <a href="blank-page.html"><i class="fa fa-fw fa-file"></i> Comments</a>
