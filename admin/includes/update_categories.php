8c81bce8905130b188ad42cb51e2d3b28061b7f2
ahmed_khaled1 <ahmedkhaledmohamed9@gmail.com>
2021-04-25 09:14:01 +0200

    CRUD operations for posts and they are working dynamically from the admin side

--- admin/includes/update_categories.php
+++ admin/includes/update_categories.php
@@ -4,22 +4,21 @@
                                 <label for="cat_title"> Edit Category</label>
                                 <?php 
                                  if (isset($_GET['edit'])) {
-                                     $cat_id=$_GET['edit'];
-                                     $query = "SELECT * FROM categories WHERE cat_id=$cat_id ";
-                                     $select_categories= mysqli_query($connection, $query);
-                                   
-                                   
-                                     while ($row = mysqli_fetch_assoc($select_categories)) {
-                                         $cat_id = $row['cat_id'];
-                                         $cat_title = $row['cat_title']; ?>
-                                     <input value="<?php if (isset($cat_title)) {
-                                             echo $cat_title;
+                                 $cat_id=$_GET['edit'];
+                                 $query = "SELECT * FROM categories WHERE cat_id=$cat_id ";
+                                 $select_categories_id= mysqli_query($connection, $query);
+                                
+                                
+                                 while ($row = mysqli_fetch_assoc($select_categories_id)) {
+                                  $cat_id = $row['cat_id'];
+                                  $cat_title = $row['cat_title']; ?>
+                                  <input value="<?php if (isset($cat_title)) {
+                                  echo $cat_title;
                                          } ?>" class="form-control" type="text" name="cat_title">
                                 <?php
                                      }
                                  } ?>
-
-                          <?php
+                                 <?php
                           // update query
 
                             if (isset($_POST['update_category'])) {
@@ -32,8 +31,7 @@
                             
                             ?>
 
-  
-                               
+
 
                                 <!-- <input class="form-control" type="text" name="cat_title"> -->
                             </div>
